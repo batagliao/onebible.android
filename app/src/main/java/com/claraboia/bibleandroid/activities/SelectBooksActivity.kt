3062d2ce8288b9095ac82c4a2d84ae90b20337e9
Lucas Batagliao <lucasbatagliao@Lucass-MacBook-Pro.local>
2016-09-28 20:11:06 -0300

    select book activity going on

--- app/src/main/java/com/claraboia/bibleandroid/activities/SelectBooksActivity.kt
+++ app/src/main/java/com/claraboia/bibleandroid/activities/SelectBooksActivity.kt
@@ -2,6 +2,8 @@ package com.claraboia.bibleandroid.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.MenuItem
 import com.claraboia.bibleandroid.R
 import kotlinx.android.synthetic.main.activity_select_books.*
 
@@ -16,5 +18,27 @@ class SelectBooksActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(true)
         //supportActionBar?.setDisplayShowCustomEnabled(false)
+
+        // Calculate ActionBar height
+        val tv = TypedValue()
+        if (theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            val actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, resources.displayMetrics)
+            val params = barBellowToolbar.layoutParams
+            params.height = actionBarHeight * 2 - 44
+            barBellowToolbar.layoutParams = params
+        }
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                super.onBackPressed()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
