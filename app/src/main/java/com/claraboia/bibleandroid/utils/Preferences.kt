df9f978d7159e8143c8419579d135cc0c1d40c44
Lucas Batagliao <lucasbatagliao@Lucass-MacBook-Pro.local>
2016-10-16 20:21:03 -0200

    Preferences saved and loaded for book selection

--- app/src/main/java/com/claraboia/bibleandroid/utils/Preferences.kt
+++ app/src/main/java/com/claraboia/bibleandroid/utils/Preferences.kt
@@ -4,17 +4,22 @@ import android.content.Context
 import android.content.ContextWrapper
 import com.claraboia.bibleandroid.R
 import com.claraboia.bibleandroid.models.BibleAddress
+import com.claraboia.bibleandroid.views.BooksSelectDisplay
+import com.claraboia.bibleandroid.views.BooksSelectSortOrder
+import com.claraboia.bibleandroid.views.BooksSelectSortType
 import com.google.gson.Gson
 import com.pixplicity.easyprefs.library.Prefs
 
 // preferences consts
-private const val SELECTED_TRANSLATION_KEY: String = "selectedTranslation"
+private const val SELECTED_TRANSLATION_KEY = "selectedTranslation"
+private const val LAST_ACCESSED_ADDRESS_KEY = "lastAccessedAddress"
+private const val BOOK_SELECTION_DISPLAY_TYPE = "BOOK_SELECTION_DISPLAY_TYPE"
+private const val BOOK_SELECTION_SORT_TYPE = "BOOK_SELECTION_SORT_TYPE"
+private const val BOOK_SELECTION_SORT_ORDER = "BOOK_SELECTION_SORT_ORDER"
 
-private const val LAST_ACCESSED_ADDRESS_KEY: String = "lastAccessedAddress"
-
-class Preferences(private val context: Context ) {
+class Preferences(private val context: Context) {
     init {
-       Prefs.Builder()
+        Prefs.Builder()
                 .setContext(context)
                 .setMode(ContextWrapper.MODE_PRIVATE)
                 .setPrefsName(context.packageName)
@@ -31,25 +36,45 @@ class Preferences(private val context: Context ) {
     var lastAccessedAddress: BibleAddress
         get() {
             val json = Prefs.getString(LAST_ACCESSED_ADDRESS_KEY, "")
-            if(json == ""){
+            if (json == "") {
                 return BibleAddress()
-            }else{
-               val gson = Gson()
+            } else {
+                val gson = Gson()
                 return gson.fromJson(json, BibleAddress::class.java)
             }
         }
-    set(value) {
-        val gson = Gson()
-        val json = gson.toJson(value)
-        Prefs.putString( LAST_ACCESSED_ADDRESS_KEY, json)
-    }
+        set(value) {
+            val gson = Gson()
+            val json = gson.toJson(value)
+            Prefs.putString(LAST_ACCESSED_ADDRESS_KEY, json)
+        }
+
+    var bookSelectionDisplayType: BooksSelectDisplay.BookLayoutDisplayType
+        get() {
+            val name = Prefs.getString(BOOK_SELECTION_DISPLAY_TYPE, BooksSelectDisplay.BookLayoutDisplayType.GRID.name)
+            return BooksSelectDisplay.BookLayoutDisplayType.valueOf(name)
+        }
+        set(value) = Prefs.putString(BOOK_SELECTION_DISPLAY_TYPE, value.name)
+
+    var bookSelectionSortType: BooksSelectSortType.BookSortType
+        get() {
+            val name = Prefs.getString(BOOK_SELECTION_SORT_TYPE, BooksSelectSortType.BookSortType.NORMAL.name)
+            return BooksSelectSortType.BookSortType.valueOf(name)
+        }
+        set(value) = Prefs.putString(BOOK_SELECTION_SORT_TYPE, value.name)
+
+
+    var bookSelectionSortOrder: BooksSelectSortOrder.BookSortOrder
+        get() {
+            val name = Prefs.getString(BOOK_SELECTION_SORT_ORDER, BooksSelectSortOrder.BookSortOrder.ASC.name)
+            return BooksSelectSortOrder.BookSortOrder.valueOf(name)
+        }
+        set(value) = Prefs.putString(BOOK_SELECTION_SORT_ORDER, value.name)
+
 
     private fun getResource(id: Int): String {
         return this.context.resources.getString(id)
     }
-
-
-
 
 
 }
