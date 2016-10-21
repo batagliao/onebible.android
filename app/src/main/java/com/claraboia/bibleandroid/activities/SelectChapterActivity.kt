c1cb55db88493b389ecca8b3e580477536473407
Lucas Lemos Bataglião <lucas.batagliao@consinco.com.br>
2016-10-21 15:50:01 -0200

    chapter activity

--- app/src/main/java/com/claraboia/bibleandroid/activities/SelectChapterActivity.kt
+++ app/src/main/java/com/claraboia/bibleandroid/activities/SelectChapterActivity.kt
@@ -1,11 +1,14 @@
 package com.claraboia.bibleandroid.activities
 
+import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import com.claraboia.bibleandroid.R
 import com.claraboia.bibleandroid.adapters.ChapterSelectionAdapter
 import com.claraboia.bibleandroid.bibleApplication
@@ -47,6 +50,48 @@ class SelectChapterActivity : AppCompatActivity() {
             }
         }
         chapterList.layoutManager = gridlayout
+        val metrics = this.baseContext.resources.displayMetrics
+        val space =  (metrics.density * 12).toInt() // resources.getDimensionPixelSize(R.dimen.activity_horizontal_margin)
+        chapterList.addItemDecoration(GridSpacingItemDecoration(5, space, true, 1))
     }
 
+    /**
+     * Created by Android Studio
+     * User: Ailurus(ailurus@foxmail.com)
+     * Date: 2015-10-28
+     * Time: 15:20
+     */
+     inner class GridSpacingItemDecoration(private val spanCount: Int, private val spacing: Int, private val includeEdge: Boolean, private val headerNum: Int): RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State ) {
+            val position = parent.getChildAdapterPosition(view) - headerNum // item position
+
+            if (position >= 0) {
+                val column = position % spanCount // item column
+
+                if (includeEdge) {
+                    outRect.left = spacing - column * spacing / spanCount // spacing - column * ((1f / spanCount) * spacing)
+                    outRect.right = (column + 1) * spacing / spanCount // (column + 1) * ((1f / spanCount) * spacing)
+
+                    if (position < spanCount) { // top edge
+                        outRect.top = spacing
+                    }
+                    outRect.bottom = spacing // item bottom
+                } else {
+                    outRect.left = column * spacing / spanCount // column * ((1f / spanCount) * spacing)
+                    outRect.right = spacing - (column + 1) * spacing / spanCount // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                    if (position >= spanCount) {
+                        outRect.top = spacing // item top
+                    }
+                }
+            } else {
+                outRect.left = 0
+                outRect.right = 0
+                outRect.top = 0
+                outRect.bottom = 0
+            }
+        }
+    }
 }
+
+
