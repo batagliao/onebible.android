d962aa05c5ff5bc18e71adfac2be14a18790f2f5
Lucas Batagliao <lucasbatagliao@Lucass-MacBook-Pro.local>
2016-10-27 21:13:30 -0200

    firebase database started

--- app/src/main/java/com/claraboia/bibleandroid/fragments/CloudTranslationsFragment.kt
+++ app/src/main/java/com/claraboia/bibleandroid/fragments/CloudTranslationsFragment.kt
@@ -6,13 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.claraboia.bibleandroid.R
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 /**
  * Created by lucasbatagliao on 26/10/16.
  */
-class CloudTranslationsFragment: Fragment() {
+class CloudTranslationsFragment : Fragment() {
+
+    private val database by lazy { FirebaseDatabase.getInstance().reference }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_cloud_translations, container, false)
+        val view = inflater?.inflate(R.layout.fragment_cloud_translations, container, false)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val query = database.child("translations").orderByKey()
+
+        //TODO: adapter goes here
     }
 }
