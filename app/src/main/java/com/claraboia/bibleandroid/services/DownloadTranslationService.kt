7a16cacd72ce3e0753236f89f154614097137c5b
Lucas Lemos Bataglião <lucas.batagliao@consinco.com.br>
2016-11-01 09:35:26 -0200

    intentservice for download translation

---
+++ app/src/main/java/com/claraboia/bibleandroid/services/DownloadTranslationService.kt
@@ -0,0 +1,15 @@
+package com.claraboia.bibleandroid.services
+
+import android.app.IntentService
+import android.content.Intent
+
+/**
+ * Created by lucas.batagliao on 01-Nov-16.
+ */
+class DownloadTranslationService : IntentService("DownloadTranslationService") {
+    override fun onHandleIntent(intent: Intent?) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        //TODO: notify on status bar when download finishes
+    }
+}
