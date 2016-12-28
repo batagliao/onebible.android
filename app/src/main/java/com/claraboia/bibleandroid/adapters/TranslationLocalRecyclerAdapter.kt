144a9a8ea6dce2bec1d71568129858cbb18a24f4
Lucas Lemos Bataglião <lucas.batagliao@consinco.com.br>
2016-12-28 12:43:52 -0200

    gradle update

--- app/src/main/java/com/claraboia/bibleandroid/adapters/TranslationLocalRecyclerAdapter.kt
+++ app/src/main/java/com/claraboia/bibleandroid/adapters/TranslationLocalRecyclerAdapter.kt
@@ -1,9 +1,12 @@
 package com.claraboia.bibleandroid.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.claraboia.bibleandroid.R
 import com.claraboia.bibleandroid.models.BibleTranslation
+import kotlinx.android.synthetic.main.layout_translation_local_item.view.*
 
 /**
  * Created by lucasbatagliao on 23/12/16.
@@ -12,17 +15,21 @@ class TranslationLocalRecyclerAdapter(val translations: List<BibleTranslation>) 
     inner class TranslationViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(translation: BibleTranslation){
-
+            itemView.item_translationName.text = translation.name
+            itemView.item_translationAbbrev.text = translation.abbreviation
+            itemView.item_translationSize.text = translation.fileSize.toString() + "MB"
         }
     }
 
 
     override fun getItemCount(): Int {
-        return translations.size;
+        return translations.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TranslationViewHolder {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inflater = LayoutInflater.from(parent?.context)
+        val view = inflater.inflate(R.layout.layout_translation_local_item, parent, false)
+        return TranslationViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TranslationViewHolder?, position: Int) {
